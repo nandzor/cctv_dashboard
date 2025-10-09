4d79fadbb39457e146cab451e44602efe58d100e
Nandzo O'Conner <nandz.id@gmail.com>
2025-10-09 13:52:28 +0700

    Update device statistics card icon in dashboard: Changed the icon for the "Total Devices" stat card to improve visual representation and user experience.

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -34,7 +34,7 @@
 
       <x-stat-card title="Total Branches" :value="$totalBranches" color="green" :icon="'<path stroke-linecap=\'round\' stroke-linejoin=\'round\' stroke-width=\'2\' d=\'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4\'/>'" />
 
-      <x-stat-card title="Total Devices" :value="$totalDevices" color="purple" :icon="'<path stroke-linecap=\'round\' stroke-linejoin=\'round\' stroke-width=\'2\' d=\'M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z\'/><path stroke-linecap=\'round\' stroke-linejoin=\'round\' stroke-width=\'2\' d=\'M15 13a3 3 0 11-6 0 3 3 0 016 0z\'/>'" />
+      <x-stat-card title="Total Devices" :value="$totalDevices" color="purple" :icon="'<path stroke-linecap=\'round\' stroke-linejoin=\'round\' stroke-width=\'2\' d=\'M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z\'/>'" />
 
       <x-stat-card title="Today's Detections" :value="$todayDetections" color="orange" :icon="'<path stroke-linecap=\'round\' stroke-linejoin=\'round\' stroke-width=\'2\' d=\'M15 12a3 3 0 11-6 0 3 3 0 016 0z\'/><path stroke-linecap=\'round\' stroke-linejoin=\'round\' stroke-width=\'2\' d=\'M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z\'/>'"
         :trend="$todayDetectionTrend" :trendUp="$todayDetectionTrendUp" />
